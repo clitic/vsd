26e3865c5e8325265706cb1494c1f0ea0dce79a3
clitic <clitic21@gmail.com>
2024-05-18 21:38:33 +0530

    code redesign 1

--- vsd/src/downloader.rs
+++ vsd/src/downloader.rs
@@ -24,6 +24,164 @@ use vsd_mp4::{
     text::{ttml_text_parser, Mp4TtmlParser, Mp4VttParser},
 };
 
+struct InputMetadata {
+    pl_type: Option<PlaylistType>,
+    text: String,
+    url: Url,
+}
+
+impl InputMetadata {
+    fn fetch(&mut self, client: &Client) -> Result<()> {
+        let response = client.get(self.url.as_ref()).send()?;
+        self.url = response.url().to_owned();
+
+        if let Some(content_type) = response.headers().get(header::CONTENT_TYPE) {
+            match content_type.as_bytes() {
+                b"application/dash+xml" | b"video/vnd.mpeg.dash.mpd" => {
+                    self.pl_type = Some(PlaylistType::Dash)
+                }
+                b"application/x-mpegurl" | b"application/vnd.apple.mpegurl" => {
+                    self.pl_type = Some(PlaylistType::Hls)
+                }
+                _ => (),
+            }
+        }
+
+        self.text = response.text()?;
+        self.update_pl_type_from_text();
+        Ok(())
+    }
+
+    fn update_pl_type_from_text(&mut self) {
+        if self.pl_type.is_none() {
+            if self.text.contains("<MPD") {
+                self.pl_type = Some(PlaylistType::Dash);
+            } else if self.text.contains("#EXTM3U") {
+                self.pl_type = Some(PlaylistType::Hls);
+            }
+        }
+    }
+}
+
+struct Prompts {
+    skip: bool,
+    raw: bool,
+}
+
+fn fetch_playlist(
+    base_url: Option<Url>,
+    client: &Client,
+    input: &str,
+    prompts: &Prompts,
+) -> Result<InputMetadata> {
+    let mut meta = InputMetadata {
+        pl_type: None,
+        text: String::new(),
+        url: base_url
+            .clone()
+            .unwrap_or_else(|| "https://example.com".parse::<Url>().unwrap()),
+    };
+    let path = Path::new(input);
+
+    if path.exists() {
+        if base_url.is_none() {
+            println!(
+                "    {} base url is not set",
+                "Warning".colorize("bold yellow")
+            );
+        }
+
+        if let Some(ext) = path.extension() {
+            let ext = ext.to_string_lossy();
+            if ext == "mpd" {
+                meta.pl_type = Some(PlaylistType::Dash);
+            } else if ext == "m3u" || ext == "m3u8" {
+                meta.pl_type = Some(PlaylistType::Hls);
+            }
+        }
+
+        meta.text = std::fs::read_to_string(path)?;
+        meta.update_pl_type_from_text();
+    } else {
+        meta.url = input.parse::<Url>().unwrap();
+        meta.fetch(&client)?;
+
+        if meta.pl_type.is_none() {
+            fetch_from_website(client, &mut meta, prompts)?;
+        }
+    }
+
+    Ok(meta)
+}
+
+fn fetch_from_website(client: &Client, meta: &mut InputMetadata, prompts: &Prompts) -> Result<()> {
+    println!(
+        "   {} website for DASH and HLS playlists",
+        "Scraping".colorize("bold cyan")
+    );
+
+    let links = utils::scrape_playlist_links(&meta.text);
+
+    match links.len() {
+        0 => bail!(utils::scrape_playlist_msg(meta.url.as_str())),
+        1 => {
+            println!("      {} {}", "Found".colorize("bold green"), &links[0]);
+            meta.url = links[0].parse::<Url>()?;
+        }
+        _ => {
+            if prompts.skip || prompts.raw {
+                println!("Select one playlist:");
+
+                for (i, link) in links.iter().enumerate() {
+                    println!("{:2}) [{}] {}", i + 1, if i == 0 { 'x' } else { ' ' }, link);
+                }
+
+                println!("------------------------------");
+
+                let mut index = 0;
+
+                if prompts.raw && !prompts.skip {
+                    print!(
+                        "Press enter to proceed with defaults.\n\
+                    Or select playlist to download (1, 2, etc.): "
+                    );
+                    std::io::stdout().flush()?;
+                    let mut input = String::new();
+                    std::io::stdin().read_line(&mut input)?;
+
+                    println!("------------------------------");
+
+                    let input = input.trim();
+
+                    if !input.is_empty() {
+                        index = input
+                            .parse::<usize>()
+                            .map_err(|_| anyhow!("input is not a valid positive number."))?
+                            - 1;
+                    }
+                }
+
+                meta.url = links
+                    .get(index)
+                    .ok_or_else(|| anyhow!("selected playlist is out of index bounds."))?
+                    .parse::<Url>()?;
+                println!("   {} {}", "Selected".colorize("bold green"), meta.url);
+            } else {
+                let question = requestty::Question::select("scraped-link")
+                    .message("Select one playlist")
+                    .should_loop(false)
+                    .choices(links)
+                    .build();
+                let answer = requestty::prompt_one(question)?;
+                meta.url = answer.as_list_item().unwrap().text.parse::<Url>()?;
+            }
+        }
+    }
+
+    meta.fetch(client)?;
+    Ok(())
+}
+
 #[allow(clippy::too_many_arguments)]
 pub(crate) fn download(
     all_keys: bool,
@@ -43,165 +201,11 @@ pub(crate) fn download(
     retry_count: u8,
     threads: u8,
 ) -> Result<()> {
-    let mut playlist_url = base_url
-        .clone()
-        .unwrap_or_else(|| "https://example.com".parse::<Url>().unwrap());
-
-    // -----------------------------------------------------------------------------------------
-    // Fetch Playlist
-    // -----------------------------------------------------------------------------------------
-
-    let mut playlist_type = None;
-    let path = Path::new(input);
-
-    let playlist = if path.exists() {
-        if base_url.is_none() {
-            println!(
-                "    {} base url is not set",
-                "Warning".colorize("bold yellow")
-            );
-        }
-
-        if let Some(ext) = path.extension() {
-            let ext = ext.to_string_lossy();
-            if ext == "mpd" {
-                playlist_type = Some(PlaylistType::Dash);
-            } else if ext == "m3u" || ext == "m3u8" {
-                playlist_type = Some(PlaylistType::Hls);
-            }
-        }
-
-        let text = std::fs::read_to_string(path)?;
-
-        if playlist_type.is_none() {
-            if text.contains("<MPD") {
-                playlist_type = Some(PlaylistType::Dash);
-            } else if text.contains("#EXTM3U") {
-                playlist_type = Some(PlaylistType::Hls);
-            }
-        }
-
-        text
-    } else {
-        let response = client.get(input).send()?;
-        playlist_url = response.url().to_owned();
-
-        if let Some(content_type) = response.headers().get(header::CONTENT_TYPE) {
-            match content_type.as_bytes() {
-                b"application/dash+xml" | b"video/vnd.mpeg.dash.mpd" => {
-                    playlist_type = Some(PlaylistType::Dash)
-                }
-                b"application/x-mpegurl" | b"application/vnd.apple.mpegurl" => {
-                    playlist_type = Some(PlaylistType::Hls)
-                }
-                _ => (),
-            }
-        }
-
-        let text = response.text()?;
-
-        if playlist_type.is_none() {
-            if text.contains("<MPD") {
-                playlist_type = Some(PlaylistType::Dash);
-            } else if text.contains("#EXTM3U") {
-                playlist_type = Some(PlaylistType::Hls);
-            }
-        }
-
-        if playlist_type.is_none() {
-            println!(
-                "   {} website for DASH and HLS playlists",
-                "Scraping".colorize("bold cyan")
-            );
-
-            let links = utils::scrape_playlist_links(&text);
-
-            match links.len() {
-                0 => bail!(utils::scrape_playlist_msg(playlist_url.as_str())),
-                1 => {
-                    println!("      {} {}", "Found".colorize("bold green"), &links[0]);
-                    playlist_url = links[0].parse::<Url>()?;
-                }
-                _ => {
-                    if skip_prompts || raw_prompts {
-                        println!("Select one playlist:");
-
-                        for (i, link) in links.iter().enumerate() {
-                            println!("{:2}) [{}] {}", i + 1, if i == 0 { 'x' } else { ' ' }, link);
-                        }
-
-                        println!("------------------------------");
-
-                        let mut index = 0;
-
-                        if raw_prompts && !skip_prompts {
-                            print!(
-                                "Press enter to proceed with defaults.\n\
-                                Or select playlist to download (1, 2, etc.): "
-                            );
-                            std::io::stdout().flush()?;
-                            let mut input = String::new();
-                            std::io::stdin().read_line(&mut input)?;
-
-                            println!("------------------------------");
-
-                            let input = input.trim();
-
-                            if !input.is_empty() {
-                                index = input.parse::<usize>().map_err(|_| {
-                                    anyhow!("input is not a valid positive number.")
-                                })? - 1;
-                            }
-                        }
-
-                        playlist_url = links
-                            .get(index)
-                            .ok_or_else(|| anyhow!("selected playlist is out of index bounds."))?
-                            .parse::<Url>()?;
-                        println!("   {} {}", "Selected".colorize("bold green"), playlist_url);
-                    } else {
-                        let question = requestty::Question::select("scraped-link")
-                            .message("Select one playlist")
-                            .should_loop(false)
-                            .choices(links)
-                            .build();
-                        let answer = requestty::prompt_one(question)?;
-                        playlist_url = answer.as_list_item().unwrap().text.parse::<Url>()?;
-                    }
-                }
-            }
-
-            let response = client.get(playlist_url).send()?;
-            playlist_url = response.url().to_owned();
-
-            if let Some(content_type) = response.headers().get(header::CONTENT_TYPE) {
-                match content_type.as_bytes() {
-                    b"application/dash+xml" | b"video/vnd.mpeg.dash.mpd" => {
-                        playlist_type = Some(PlaylistType::Dash)
-                    }
-                    b"application/x-mpegurl" | b"application/vnd.apple.mpegurl" => {
-                        playlist_type = Some(PlaylistType::Hls)
-                    }
-                    _ => (),
-                }
-            }
-
-            let text = response.text()?;
-
-            if playlist_type.is_none() {
-                if text.contains("<MPD") {
-                    playlist_type = Some(PlaylistType::Dash);
-                } else if text.contains("#EXTM3U") {
-                    playlist_type = Some(PlaylistType::Hls);
-                }
-            }
-
-            text
-        } else {
-            text
-        }
-    };
-
+    let prompts = Prompts { skip: skip_prompts, raw: raw_prompts };
+    let meta = fetch_playlist(base_url.clone(), &client, input, &prompts)?;
+    let playlist_type = meta.pl_type;
+    let playlist = meta.text;
+    let playlist_url = meta.url;
     // -----------------------------------------------------------------------------------------
     // Parse Playlist & Select Streams & Push Segments
     // -----------------------------------------------------------------------------------------
