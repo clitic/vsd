f5e3472c60ea426a307e6a121c89ccc93c7da0d2
clitic <clitic21@gmail.com>
2026-01-11 22:12:15 +0530

    basic lib support

--- vsd/src/lib.rs
+++ vsd/src/lib.rs
@@ -13,152 +13,161 @@ mod utils;
 pub use commands::Args;
 pub use reqwest;
 
-// use anyhow::Result;
-// use downloader::{MAX_RETRIES, MAX_THREADS, SKIP_MERGE, Decrypter};
-// use reqwest::{Client, Url};
-// use std::path::PathBuf;
+use crate::{
+    automation::{InteractionType, SelectOptions},
+    downloader::{Decrypter, MAX_RETRIES, MAX_THREADS, SKIP_DECRYPT, SKIP_MERGE},
+};
+use anyhow::{Ok, Result};
+use reqwest::{Client, Url};
+use std::{collections::HashMap, path::PathBuf, sync::atomic::Ordering};
 
-// /// A downloader for DASH and HLS playlists.
-// pub struct Downloader {
-//     pub input: String,
-//     pub base_url: Option<Url>,
-//     pub directory: Option<PathBuf>,
-//     pub output: Option<PathBuf>,
-//     pub subs_codec: String,
-//     pub interactive: bool,
-//     pub interactive_raw: bool,
-//     pub select_streams: String,
-//     pub keys: Decrypter,
-//     pub no_decrypt: bool,
-//     pub retries: u8,
-//     pub threads: u8,
-//     pub client: Client,
-// }
+/// A downloader for DASH and HLS playlists.
+pub struct Downloader {
+    client: Client,
+    input: String,
+    base_url: Option<Url>,
+    directory: Option<PathBuf>,
+    output: Option<PathBuf>,
+    subs_codec: String,
+    select_options: SelectOptions,
+    decrypter: Decrypter,
+}
 
-// impl Downloader {
-//     pub fn new(client: Client, input: String) -> Self {
-//         Self {
-//             input,
-//             base_url: None,
-//             directory: None,
-//             output: None,
-//             subs_codec: "copy".to_string(),
-//             interactive: false,
-//             interactive_raw: false,
-//             select_streams: "v=best:s=en".to_string(),
-//             keys: Decrypter::None,
-//             no_decrypt: false,
-//             no_merge: false,
-//             retries: 5,
-//             threads: 5,
-//             client,
-//         }
-//     }
+impl Downloader {
+    pub fn new(client: Client, input: String) -> Self {
+        Self {
+            client,
+            input,
+            base_url: None,
+            directory: None,
+            output: None,
+            subs_codec: "copy".to_owned(),
+            select_options: SelectOptions::parse("v=best:s=en"),
+            decrypter: Decrypter::None,
+        }
+    }
 
-//     pub fn base_url(mut self, base_url: impl Into<Url>) -> Self {
-//         self.base_url = Some(base_url.into());
-//         self
-//     }
+    /// Base url to be used for building absolute url to segment.
+    /// This flag is usually needed for local input files.
+    /// By default redirected playlist url is used.
+    pub fn base_url(mut self, base_url: impl Into<Url>) -> Self {
+        self.base_url = Some(base_url.into());
+        self
+    }
 
-//     pub fn directory(mut self, directory: impl Into<PathBuf>) -> Self {
-//         self.directory = Some(directory.into());
-//         self
-//     }
+    /// Change directory path for temporarily downloaded files.
+    /// By default current working directory is used.
+    pub fn directory(mut self, directory: impl Into<PathBuf>) -> Self {
+        self.directory = Some(directory.into());
+        self
+    }
 
-//     pub fn output(mut self, output: impl Into<PathBuf>) -> Self {
-//         self.output = Some(output.into());
-//         self
-//     }
+    /// Mux all downloaded streams to a video container (.mp4, .mkv, etc.) using ffmpeg.
+    /// Note that existing files will be overwritten and downloaded streams will be deleted.
+    pub fn output(mut self, output: impl Into<PathBuf>) -> Self {
+        self.output = Some(output.into());
+        self
+    }
 
-//     pub fn subs_codec(mut self, subs_codec: impl Into<String>) -> Self {
-//         self.subs_codec = subs_codec.into();
-//         self
-//     }
+    /// Force some specific subtitle codec when muxing through ffmpeg.
+    /// By default `mov_text` is used for .mp4 and `copy` for others.
+    pub fn subs_codec(mut self, subs_codec: impl Into<String>) -> Self {
+        self.subs_codec = subs_codec.into();
+        self
+    }
 
-//     pub fn interactive(mut self, raw: bool) -> Self {
-//         self.interactive = interactive;
-//         self
-//     }
+    /// Prompt for custom streams selection with modern style input prompts. By default proceed with defaults.
+    /// Prompt for custom streams selection with raw style input prompts. By default proceed with defaults.
+    pub fn interactive(self, raw: bool) -> Self {
+        automation::set_interaction_type(if raw {
+            InteractionType::Raw
+        } else {
+            InteractionType::Modern
+        });
+        self
+    }
 
-//     /// Sets the stream selection filters.
-//     pub fn select_streams(mut self, select_streams: impl Into<String>) -> Self {
-//         self.select_streams = select_streams.into();
-//         self
-//     }
+    /// Filters to be applied for automatic stream selection.
+    pub fn select_streams(mut self, select_streams: &str) -> Self {
+        self.select_options = SelectOptions::parse(select_streams);
+        self
+    }
 
-//     /// Consumes the builder and returns a configured [`Downloader`].
-//     pub fn build(self) -> Downloader {
-//         Downloader {
-//             input: self.input,
-//             base_url: self.base_url,
-//             directory: self.directory,
-//             output: self.output,
-//             subs_codec: self.subs_codec,
-//             interactive: self.interactive,
-//             interactive_raw: self.interactive_raw,
-//             select_streams: self.select_streams,
-//             keys: self.keys,
-//             no_decrypt: self.no_decrypt,
-//             no_merge: self.no_merge,
-//             retries: self.retries,
-//             threads: self.threads,
-//             client: self.client,
-//         }
-//     }
-// }
+    /// Keys for decrypting encrypted streams.
+    /// KID:KEY should be specified in hex format.
+    pub fn keys(mut self, keys: &HashMap<String, String>) -> Self {
+        let mut kid_key_pairs = HashMap::new();
 
-// // pub async fn execute(client: &Client) -> Result<()> {
-// //     MAX_RETRIES.store(self.retries, Ordering::SeqCst);
-// //     MAX_THREADS.store(self.threads, Ordering::SeqCst);
-// //     SKIP_MERGE.store(self.no_merge, Ordering::SeqCst);
+        for (kid, key) in keys {
+            let kid = kid.to_ascii_lowercase().replace('-', "");
+            let key = key.to_ascii_lowercase().replace('-', "");
 
-// //     let client = self.client()?;
+            if kid.len() == 32
+                && key.len() == 32
+                && kid.chars().all(|c| c.is_ascii_hexdigit())
+                && key.chars().all(|c| c.is_ascii_hexdigit())
+            {
+                kid_key_pairs.insert(kid, key);
+            }
+        }
 
-// //     let prompter = Prompter {
-// //         interactive: self.interactive,
-// //         interactive_raw: self.interactive_raw,
-// //     };
+        self.decrypter = Decrypter::Mp4Decrypt(kid_key_pairs);
+        self
+    }
 
-// //     let meta = downloader::fetch_playlist(
-// //         self.base_url.clone(),
-// //         &client,
-// //         &self.input,
-// //         &prompter,
-// //         &self.query,
-// //     )
-// //     .await?;
+    /// Download encrypted streams without decrypting them.
+    /// Note that --output flag is ignored if this flag is used.
+    pub fn skip_decrypt(self, skip_decrypt: bool) -> Self {
+        SKIP_DECRYPT.store(skip_decrypt, Ordering::SeqCst);
+        self
+    }
 
-// //     if self.list_streams {
-// //         downloader::list_all_streams(&meta)?;
-// //     } else if self.parse {
-// //         let playlist =
-// //             downloader::parse_all_streams(self.base_url.clone(), &client, &meta, &self.query)
-// //                 .await?;
-// //         serde_json::to_writer(std::io::stdout(), &playlist)?;
-// //     } else {
-// //         let streams = downloader::parse_selected_streams(
-// //             self.base_url.clone(),
-// //             &client,
-// //             &meta,
-// //             &prompter,
-// //             &self.query,
-// //             SelectOptions::parse(&self.select_streams),
-// //         )
-// //         .await?;
+    /// Download streams without merging them.
+    /// Note that --output flag is ignored if this flag is used.
+    pub fn skip_merge(self, skip_merge: bool) -> Self {
+        SKIP_MERGE.store(skip_merge, Ordering::SeqCst);
+        self
+    }
 
-// //         downloader::download(
-// //             self.base_url,
-// //             client,
-// //             self.keys,
-// //             self.directory,
-// //             self.no_decrypt,
-// //             self.output,
-// //             self.query,
-// //             streams,
-// //             self.subs_codec,
-// //         )
-// //         .await?;
-// //     }
+    /// Maximum number of retries to download an individual segment.
+    pub fn max_retries(self, max_retries: u8) -> Self {
+        MAX_RETRIES.store(max_retries, Ordering::SeqCst);
+        self
+    }
 
-// //     Ok(())
-// // }
+    /// Maximum number of retries to download an individual segment.
+    pub fn max_threads(self, max_threads: u8) -> Self {
+        MAX_THREADS.store(max_threads, Ordering::SeqCst);
+        self
+    }
+
+    pub async fn download(self) -> Result<()> {
+        let query = HashMap::new();
+        let meta =
+            downloader::fetch_playlist(self.base_url.clone(), &self.client, &self.input, &query)
+                .await?;
+
+        let streams = downloader::parse_selected_streams(
+            self.base_url.clone(),
+            &self.client,
+            &meta,
+            &query,
+            self.select_options,
+        )
+        .await?;
+
+        downloader::download(
+            self.base_url,
+            self.client,
+            self.decrypter,
+            self.directory,
+            self.output,
+            query,
+            streams,
+            self.subs_codec,
+        )
+        .await?;
+    
+        Ok(())
+    }
+}
